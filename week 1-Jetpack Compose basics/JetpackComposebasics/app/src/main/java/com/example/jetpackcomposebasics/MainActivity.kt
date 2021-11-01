d4f5470242e4b6ea97cc55316da2293c132a1261
sslee <sangsulee@ebay.com>
2021-11-02 00:29:30 +0900

    Feat: OutlinedButton 사용

--- week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -4,10 +4,9 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -37,12 +36,19 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Text(text = "Hello,")
-            Text(text = "$name!")
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
         }
     }
 }
+
 
 @Composable
 private fun MyApp(names: List<String> = listOf("World", "Compose")) {
@@ -52,6 +58,8 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
         }
     }
 }
+
+
 
 @Preview(showBackground = true, name = "Text preview", widthDp = 320)
 @Composable
