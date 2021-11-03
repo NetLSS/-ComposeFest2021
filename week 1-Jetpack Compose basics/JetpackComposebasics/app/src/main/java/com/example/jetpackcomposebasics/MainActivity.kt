b54612b1328dcd9ed09999c35a9f333ca4e4fa13
sslee <sangsulee@ebay.com>
2021-11-03 23:35:42 +0900

    Feat: 초빠른 LazyColumn 사용해보기 🔥

--- week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -85,12 +87,12 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) { // for문 을 사용해서 ui 를 구성할 수 있다는 것을 보여줌
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->  // LazyColumn 사용 (리사이클러뷰 대응)
             Greeting(name = name)
         }
-    }
+    } // 하지만 재활용은 하지 않음. 단지 컴포저블 될뿐. 더욱 저렴하다.
 }
 
 @Composable
