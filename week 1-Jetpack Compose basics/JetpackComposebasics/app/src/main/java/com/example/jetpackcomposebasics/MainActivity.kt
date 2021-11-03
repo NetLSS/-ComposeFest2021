ddfd7d2f3faf7fc9c7dc0aa3674a24a6e34e3bf6
sslee <sangsulee@ebay.com>
2021-11-03 17:58:06 +0900

    Chore: add comment

--- week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ week 1-Jetpack Compose basics/JetpackComposebasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -24,8 +24,10 @@ class MainActivity : ComponentActivity() {
 //                Surface(color = MaterialTheme.colors.primary) {
 //                    Greeting("Android")
 //                }
+                // 온보딩 페이지를 보여주기 위해서 (위 코드)는 주석처리
+                // 왜 주석? shouldShowOnboarding 을 사용해야 되기 때문임.
 
-                MyApp()
+                MyApp()  // MyApp 이 초기에 보여지는 거지
             }
         }
     }
@@ -34,14 +36,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
 
+    // remember 를 붙여주어야 변경되었을 때 UI 변경이 이루어진다.
     val expanded = remember { mutableStateOf(false) }
 
+    // expanded 가 remember 이기 때문에 expanded 변경 시 extraPadding 도 변경된다.
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
+    // Surface 가 ui 홀더 같은 느낌?? 인것 같다. 여기 안에 여러가지를 담을 수 있는거지
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
+        // 가로 배치를 하려고 Row 사용 (이렇게 하면 버튼 달린 네임텍(?) 하나가 생성되는 거지)
+        // 이를 여러개 생성하면(Greetings) Greeting 리스트로 보여줄 수 있는것
+        // 가로배치는 이런거지 예를 들면 Hello, World 오른쪽에 버튼이 붙는 것
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
@@ -65,11 +73,12 @@ fun Greeting(name: String) {
 private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
     var shouldShowOnboarding by remember {
-        mutableStateOf(true)
+        mutableStateOf(true) // 최초 앱 실행시에는 true 로 해서 온보딩 페이지를 보여줌
     }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        // 이후 콜백에서 버튼 클릭시 이를 false 로 하면 이건 remember 니까 ui 업데이트가 자동으로 발생.
     } else{
         Greetings()
     }
@@ -78,21 +87,21 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 @Composable
 private fun Greetings(names: List<String> = listOf("World", "Compose")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+        for (name in names) { // for문 을 사용해서 ui 를 구성할 수 있다는 것을 보여줌
             Greeting(name = name)
         }
-
     }
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
+fun OnboardingScreen(onContinueClicked: () -> Unit) { // 클릭 리스너를 받음 (람다)
     // TODO: THis state Should be hoisted
     //var shouldShowOnboarding by remember { mutableStateOf(true) }
 
-    androidx.compose.material.Surface {
+    Surface {
+        // Column 으로 세로배치
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize(), // 화면 전체를 채우겠다
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -123,4 +132,10 @@ fun OnboardingPreview() {
     BasicsCodelabTheme {
         OnboardingScreen(onContinueClicked = {})
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreatingsPreview() {
+    Greetings()
 }
