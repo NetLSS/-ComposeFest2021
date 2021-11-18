713b2c95dc4ec41f4dde1ec4bc358da83b7ac86f
sslee <sangsulee@ebay.com>
2021-11-18 21:57:19 +0900

    3W-2-6: transitionSpec 으로 애니메이션 유별나게 만들기

--- week 3-2-Jetpack Compose Animation/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
+++ week 3-2-Jetpack Compose Animation/start/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt
@@ -486,16 +486,47 @@ private fun HomeTabIndicator(
     tabPage: TabPage
 ) {
     // TODO 4: Animate these value changes.
-    val transition = updateTransition(tabPage)
-    val indicatorLeft by transition.animateDp { page ->
+    val transition = updateTransition(
+        tabPage,
+        label = "Tab indicator"
+    )
+    val indicatorLeft by transition.animateDp(
+        transitionSpec = {
+            if (TabPage.Home isTransitioningTo TabPage.Work) {
+                // Indicator moves to the right.
+                // The left edge moves slower than the right edge.
+                spring(stiffness = Spring.StiffnessVeryLow)
+            } else {
+                // Indicator moves to the left.
+                // The left edge moves faster than the right edge.
+                spring(stiffness = Spring.StiffnessMedium)
+            }
+        },
+        label = "Indicator left"
+    ) { page ->
         tabPositions[page.ordinal].left
-    } //= tabPositions[tabPage.ordinal].left // IndicatorLeft는 탭 행에서 표시기의 왼쪽 가장자리의 수평 위치
-    val indicatorRight by transition.animateDp { page ->
+    }
+    val indicatorRight by transition.animateDp(
+        transitionSpec = {
+            if (TabPage.Home isTransitioningTo TabPage.Work) {
+                // Indicator moves to the right
+                // The right edge moves faster than the left edge.
+                spring(stiffness = Spring.StiffnessMedium)
+            } else {
+                // Indicator moves to the left.
+                // The right edge moves slower than the left edge.
+                spring(stiffness = Spring.StiffnessVeryLow)
+            }
+        },
+        label = "Indicator right"
+    ) { page ->
         tabPositions[page.ordinal].right
-    } //= tabPositions[tabPage.ordinal].right // IndicatorRight는 표시기의 오른쪽 가장자리의 수평 위치
-    val color by transition.animateColor { page ->
+    }
+    val color by transition.animateColor(
+        label = "Border color"
+    ) { page ->
         if (page == TabPage.Home) Purple700 else Green800
-    } //= if (tabPage == TabPage.Home) Purple700 else Green800
+    }
     Box(
         Modifier
             .fillMaxSize()
