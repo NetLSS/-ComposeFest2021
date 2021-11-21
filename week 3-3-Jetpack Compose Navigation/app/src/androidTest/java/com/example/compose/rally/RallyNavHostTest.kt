0864722dc158438b9a222abe1ea961533bf648fc
sslee <sangsulee@ebay.com>
2021-11-21 14:24:40 +0900

    3W-3-7: 화면 표시 단위 테스트

--- week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -1,6 +1,8 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import org.junit.Assert.fail
@@ -19,7 +21,10 @@ class RallyNavHostTest {
             navController = rememberNavController()
             RallyNavHost(navController = navController)
         }
-        fail() // TEST 를 완료하라는 TODO 역할을 함 
+        //fail() // TEST 를 완료하라는 TODO 역할을 함
+        composeTestRule
+            .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed() // 현재 해당 화면이 표시 되는지 검증 
     }
 
 
