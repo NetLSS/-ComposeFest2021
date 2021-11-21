7fbb7b4194e9c745920c18aedee3c0cef93df0d5
sslee <sangsulee@ebay.com>
2021-11-21 15:57:45 +0900

    3W-3-7: @Before 로 선작업 추출

--- week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import org.junit.Assert.fail
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,16 +16,20 @@ class RallyNavHostTest {
     val composeTestRule = createComposeRule()
     lateinit var  navController: NavHostController
 
-    @Test
-    fun rallyNavHost() {
+    @Before
+    fun setupRallyNavHost() {
         composeTestRule.setContent {
             navController = rememberNavController()
             RallyNavHost(navController = navController)
         }
+    }
+
+    @Test
+    fun rallyNavHost() {
         //fail() // TEST 를 완료하라는 TODO 역할을 함
         composeTestRule
             .onNodeWithContentDescription("Overview Screen")
-            .assertIsDisplayed() // 현재 해당 화면이 표시 되는지 검증 
+            .assertIsDisplayed() // 현재 해당 화면이 표시 되는지 검증
     }
 
 
