7f1d8b2a556a1aa70e62834ae39d48ffb4a031e8
sslee <sangsulee@ebay.com>
2021-11-22 21:06:23 +0900

    4W-1-3: 테스트에 RallyTheme 사용 하기

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -3,6 +3,7 @@ package com.example.compose.rally
 import androidx.compose.material.Text
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.example.compose.rally.ui.components.RallyTopAppBar
+import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
 import org.junit.Test
 
@@ -36,11 +37,13 @@ class TopAppBarTest {
     fun rallyTopAppBarTest() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = { /*todo*/ },
-                currentScreen = RallyScreen.Accounts
-            )
+            RallyTheme {
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = { /*todo*/ },
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
         }
         Thread.sleep(5000)
     }
