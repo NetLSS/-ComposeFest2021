c9723afab3b50f42f1a332f2c806838676b6ac03
sslee <sangsulee@ebay.com>
2021-11-22 21:11:11 +0900

    4W-1-3: 탭이 선택되었는지 확인
    Test

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,7 +1,9 @@
 package com.example.compose.rally
 
 import androidx.compose.material.Text
+import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
@@ -46,5 +48,28 @@ class TopAppBarTest {
             }
         }
         Thread.sleep(5000)
+    }
+
+    /*
+    composeTestRule{.finder}{.assertion}{.action}
+
+    For example: onNodeWithText, onNodeWithContentDescription, isSelected, hasContentDescription,
+     assertIsSelected...
+     */
+
+    @Test
+    fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreen = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreen,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
     }
 }
