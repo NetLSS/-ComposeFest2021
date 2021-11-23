312a551fab5e5faa9b45addd8e462ab5d67d3059
sslee <sangsulee@ebay.com>
2021-11-23 22:37:18 +0900

    4W-2-7: ViewModel 과 연결
    이때 null 확인을 위해
    Content 컴포저블을 따로 분리하는 패턴 사용 (권장)

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -23,17 +23,24 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
 fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
-    Surface {
-        Text("Hello Compose")
+    // VM의 LiveData<Plant> 필드에서 오는 값을 관찰합니다.
+    val plant by plantDetailViewModel.plant.observeAsState()
+
+    // 식물이 null이 아닌 경우 내용을 표시합니다.
+    plant?.let { 
+        PlantDetailContent(plant = it)
     }
 }
 
@@ -47,6 +54,25 @@ view model
  호스팅하는 수명 주기 소유자 간에 동일한 인스턴스가 공유됩니다.
 
  */
+
+/*
+live data
+
+컴포저블에서 LiveData를 관찰하려면 LiveData.observeAsState() 함수를 사용하세요.
+
+참고: LiveData.observeAsState()는 LiveData 관찰을 시작하고 State 객체를 통해 값을 나타냅니다.
+LiveData에 새 값이 게시될 때마다 반환된 State가 업데이트되어 모든 State.value 사용이 재구성됩니다.
+ */
+
+/*
+LiveData에서 내보낸 값이 null일 수 있으므로 null 검사에서 사용을 래핑해야 합니다.
+그 때문에 그리고 재사용성을 위해 LiveData 소비를 분할하고 다른 구성 요소에서 수신하는 것이
+ 좋은 패턴입니다.
+ */
+@Composable
+fun PlantDetailContent(plant: Plant) {
+    PlantName(plant.name)
+}
 
 @Composable
 private fun PlantName(name: String) {
@@ -73,5 +99,14 @@ private fun PlantName(name: String) {
 private fun PlantNamePreview() {
     MaterialTheme {
         PlantName("Apple")
+    }
+}
+
+@Preview
+@Composable
+private fun PlantDetailContentPreview() {
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MaterialTheme {
+        PlantDetailContent(plant = plant)
     }
 }
